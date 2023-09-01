59edc35e0b1e5a3612cd209b6274cbe63c05f35d
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-01 10:02:18 -0300

    updated - removed firstName and lastName properties. use fullname

--- src/database/models/userSchema.js
+++ src/database/models/userSchema.js
@@ -1,36 +1,31 @@
-const mongoose = require('../index')
-const bcript = require('bcrypt')
+const mongoose = require("../index");
+const bcript = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    firstName: { 
-        type: String,
-        required: [true, "Please enter firstName"] 
-    },
-    lastName: { 
-        type: String,
-        required: [true, "Please enter lastName"]
-    },
-    email: { 
-        type: String,
-        unique: true,
-        required: [true, "Please enter email"],
-        lowercase: true
+  fullname: {
+    type: String,
+    required: [true, "Please enter fullname"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter email"],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    },
-    password: { 
-        type: String,
-        required: [true, "Please enter password"]
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+userSchema.pre("save", async function (next) {
+  const hash = await bcript.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
 
-userSchema.pre('save', async function(next){
-    const hash = await bcript.hash(this.password, 10)
-    this.password = hash
-    next()
-})
-
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
