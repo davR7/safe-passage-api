35e69ba33a82b7ba16257d6e5b623171b1d86a6b
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-01 17:16:58 -0300

    updated - middlewares and loaders imported

--- src/app.js
+++ src/app.js
@@ -1,25 +1,17 @@
-require("./config/dotenv");
+require("./config/dotenv")
 const port = 3030;
-const middlewares = require("./config/middlewares.js");
-const routes = require("./routes/routes.js");
-const app = require("express")();
+const app = require("express")()
+const loaders = require("./loaders")
+const routes = require("./routes")
+const { notFoundRoute, errorHandler } = require('./middlewares')
 
-middlewares(app);
-routes(app);
-
-app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  return res.json({ error: error.message });
-});
+loaders(app)
+routes(app)
+app.use(notFoundRoute)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`
 App running at port:
--> http://localhost:${port}`);
+-> http://localhost:${port}`)
 });
