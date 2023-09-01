5973bf77743743c715b7abe1091c8b267f77c57e
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-01 15:50:22 -0300

    imported - validatePassword, hashPassword and tokenGenerate functions

--- src/controllers/authCtl.js
+++ src/controllers/authCtl.js
@@ -1,46 +1,55 @@
-const bcript = require("bcrypt");
-const userModel = require("../database/models/userSchema");
-const { tokenGenerate } = require("../utils");
+const userModel = require("../database/models/userSchema")
+const { tokenGenerate, hashPassword, validatePassword } = require("../utils")
 
 exports.signin = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (!email && password) {
-    return res.status(400).send({ error: "Email is required" });
+    return res.status(400).send({ error: "Email is required" })
   } else if (email && !password) {
-    return res.status(400).send({ error: "Password is required" });
+    return res.status(400).send({ error: "Password is required" })
   } else if (!email && !password) {
-    return res.status(400).send({ error: "Email and Password is required" });
+    return res.status(400).send({ error: "Email and Password is required" })
   }
 
-  const dtUser = await userModel.findOne({ email });
-  if (!dtUser)
-    return res.status(400).json({ error: "Email is not registered" });
+  const user = await userModel.findOne({ email })
+  if (!user)
+    return res
+      .status(400)
+      .json({ success: false, error: "Email is not registered" })
 
-  const passIsMatch = await bcript.compare(password, dtUser.password);
-  if (!passIsMatch)
-    return res.status(400).json({ error: "Password is invalid" });
+  const isMatch = await validatePassword(password, user.password)
+  if (!isMatch)
+    return res
+      .status(400)
+      .json({ success: false, error: "Password is incorrect" })
 
-  const token = tokenGenerate({ id: dtUser.id });
-  return res.json({ success: true, token });
+  try {
+    const token = await tokenGenerate({ id: user.id });
+    return res.json({ success: true, token })
+  } catch (err) {
+    next(err)
+  }
 };
 
 exports.signup = async (req, res, next) => {
-  const user = { ...req.body };
+  const { email, password, ...rest } = req.body
 
-  if (await userModel.findOne({ email: user.email }))
+  if (await userModel.findOne({ email }))
     return res
       .status(400)
-      .json({ success: false, error: "User already exists" });
+      .json({ success: false, error: "User already exists" })
 
   try {
-    const newUser = await userModel.create(user);
-    return res.status(201).json({ success: true, user: newUser });
+    const hash = await hashPassword(password)
+    const newUser = await userModel.create({ ...rest, email, password: hash })
+    return res.status(201).json({ success: true, user: newUser })
   } catch (err) {
-    next(err);
+    next(err)
   }
 };
 
-exports.msgLogin = (req, res, next) => {
-  return res.json({ msg: "You are logged in. :)" });
+exports.msgLogin = (req, res) => {
+  const userId = req.user.id
+  return res.json({ success: true, userId })
 };
