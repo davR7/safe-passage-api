71e5bc8ec30ca6c595723ca50aecf39b35c4bbdd
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-01 09:45:52 -0300

    imported - dotenv.js file

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-const port = 3030
-const middlewares = require('./config/middlewares.js')
-const routes = require('./routes/routes.js')
-const app = require('express')()
+require("./config/dotenv");
+const port = 3030;
+const middlewares = require("./config/middlewares.js");
+const routes = require("./routes/routes.js");
+const app = require("express")();
 
-middlewares(app)
-routes(app)
+middlewares(app);
+routes(app);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500)
-  return res.json({ error: error.message })
-})
+  res.status(error.status || 500);
+  return res.json({ error: error.message });
+});
 
 app.listen(port, () => {
   console.log(`
 App running at port:
--> http://localhost:${port}`)
-})
+-> http://localhost:${port}`);
+});
