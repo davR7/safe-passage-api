780db49ae17e31a5eb8604c93f0ca2bcedcd1e13
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-02 13:46:42 -0300

    updated - removed validations (moved to middlewares)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,25 +4,13 @@ const { tokenGenerate, hashPassword, validatePassword } = require("../utils")
 exports.signin = async (req, res, next) => {
   const { email, password } = req.body
 
-  if (!email && password) {
-    return res.status(400).send({ error: "Email is required" })
-  } else if (email && !password) {
-    return res.status(400).send({ error: "Password is required" })
-  } else if (!email && !password) {
-    return res.status(400).send({ error: "Email and Password is required" })
-  }
-
   const user = await findOneUser({ email })
   if (!user)
-    return res
-      .status(400)
-      .json({ success: false, error: "Email is not registered" })
+    return res.status(400).json({ success: false, error: "Email is not registered" })
 
   const isMatch = await validatePassword(password, user.password)
   if (!isMatch)
-    return res
-      .status(400)
-      .json({ success: false, error: "Password is incorrect" })
+    return res.status(400).json({ success: false, error: "Password is incorrect" })
 
   try {
     const token = await tokenGenerate({ id: user.id });
@@ -33,23 +21,21 @@ exports.signin = async (req, res, next) => {
 };
 
 exports.signup = async (req, res, next) => {
-  const { email, password, ...rest } = req.body
+  const { fullname, email, password } = req.body
 
   if (await findOneUser({ email }))
-    return res
-      .status(400)
-      .json({ success: false, error: "User already exists" })
+    return res.status(400).json({ success: false, error: "User already exists" })
 
   try {
     const hash = await hashPassword(password)
-    const newUser = await createUser({ ...rest, email, password: hash })
+    const newUser = await createUser({ fullname, email, password: hash })
     return res.status(201).json({ success: true, user: newUser })
   } catch (err) {
     next(err)
   }
 };
 
-exports.msgLogin = (req, res) => {
+exports.readAuthUser = (req, res) => {
   const userId = req.user.id
   return res.json({ success: true, userId })
 };
