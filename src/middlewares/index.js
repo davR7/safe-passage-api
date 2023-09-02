a3efa2dbc8c49f1ab73056759b88e88e15cc8e3e
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-02 13:38:59 -0300

    updated - imported and exported validateRequest.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const notFoundRoute = require('./notFoundRoute')
 const errorHandler = require('./errorHandler')
+const validateRequest = require('./validateRequest')
 
 module.exports = {
+  errorHandler,
   notFoundRoute,
-  errorHandler
+  validateRequest,
 }
