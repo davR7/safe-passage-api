abff135d70a8d903d1b56fbeb30a68911637591e
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-02 13:31:46 -0300

    created - Joi error handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,9 @@
 module.exports = ErrorHandler = (error, req, res, next) => {
+  if (error?.name === 'ValidationError') {
+    res.status(400);
+    return res.json({ success: false, error: error.details[0].message })
+  }
+  
   res.status(error.status || 500);
   return res.json({ error: error.message });
 }
